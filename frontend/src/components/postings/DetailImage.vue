5a22a3895d02e4e2d0f2f4bb35a08a8d015476f5
oihater95 <oihater95@gmail.com>
2021-07-27 22:03:41 +0900

    code : 상세페이지 이미지 컴포넌트 구현

---
+++ frontend/src/components/postings/DetailImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="DetailImg-section" class="container d-flex">
+    <v-card
+    id="DetailImg-card"
+    class="row"
+    elevation="2"
+    max-width="1050px"
+    max-height="1050px"
+    >
+      <img 
+      id="DetailImageSection"
+      class="col-12"
+      :src="$route.params.imageSrc">
+    </v-card>
+  </div>
+</template>
+
+<script>
+// import axios from'axios'
+import "@/css/postings/DetailImage.scss"
+
+export default {
+  name: 'DetailImage',
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/postings/PostingDetail.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <div class="container">
+    <DetailImage filename="filename" imageSrc="imageSrc"/>
+    <div class="posting-icon__buttons text-right me-5 pe-5 my-5">
+      <v-btn
+        class="mx-2"
+        fab
+        small
+        outlined
+        color="green darken-3">
+        <v-icon dark>
+          mdi-expand-all-outline
+        </v-icon>
+      </v-btn>
+      <v-btn
+        class="mx-2"
+        fab
+        small
+        outlined
+        color="blue darken-3">
+        <v-icon dark>
+          mdi-image-edit-outline
+        </v-icon>
+      </v-btn>
+      <v-btn
+        class="mx-2"
+        fab
+        small
+        outlined
+        color="red darken-1">
+        <v-icon dark>
+          mdi-trash-can-outline
+        </v-icon>
+      </v-btn>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import DetailImage from '@/components/postings/DetailImage'
+
+export default {
+  name: 'PostingDetail',
+  components: {
+    DetailImage,
+  },
+  // params로 받아서 props로 받을 수 있음
+  // filename = $route.params.filename
+  // imageSrc = $route.params.imageSrc
+  props: {  
+    filename: {
+      type: String
+    },
+    imageSrc : {
+      type: String
+    }
+  },
+
+}
+
+</script>
+
+<style>
+
+</style>
