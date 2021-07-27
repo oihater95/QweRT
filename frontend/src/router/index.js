a090d640116d0d5e4e0908909189041657d41374
오승철 <seung9412@naver.com>
2021-07-27 18:14:45 +0900

    code : 에러 수정 전 큐레이션 코드 작성

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MainPage from '@/views/postings/MainPage.vue'
+import CurationPage from '@/views/curations/CurationPage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'MainPage',
     component: MainPage
+  },
+  {
+    path: '/',
+    name: 'CurationPage',
+    component: CurationPage
   },
 ]
 
