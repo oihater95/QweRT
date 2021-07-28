a6f78456380a22d12faa8c36ac2f5ab468ece3d1
오승철 <seung9412@naver.com>
2021-07-28 15:13:19 +0900

    code : font 변경

--- backend/src/main/java/com/web/qwert/controller/account/AccountController.java
+++ backend/src/main/java/com/web/qwert/controller/account/AccountController.java
@@ -36,7 +36,7 @@ import org.springframework.web.bind.annotation.RequestBody;
         @ApiResponse(code = 404, message = "Not Found", response = BasicResponse.class),
         @ApiResponse(code = 500, message = "Failure", response = BasicResponse.class) })
 
-// @CrossOrigin(origins = { "http://localhost:3000" })
+ @CrossOrigin(origins = { "http://localhost:8080" })
 @RestController
 public class AccountController {
 
