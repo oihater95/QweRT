b327e5a6434bf0ca5de102bcb67cac926624c0a4
이희준 <heejun2822@hanmail.net>
2021-08-05 14:53:51 +0900

    code : 프로필 회원 탈퇴 axios 완성

--- frontend/src/views/profiles/ModifyProfile.vue
+++ frontend/src/views/profiles/ModifyProfile.vue
@@ -89,7 +89,6 @@
       >
         <v-btn
           color="#E8D48D"
-          @click="changeUserInfo"
         >
           저장
         </v-btn>
@@ -199,7 +198,7 @@
         <v-btn
           color="#F4A380"
           dark
-          @click="signOut"
+          @click="signout"
         >
           탈퇴
         </v-btn>
@@ -219,6 +218,10 @@
     </v-row>
     <Modal
       :msg="modalMsg"
+      class="d-none"
+      @changePassword-ok-sign="password = '', newPassword = '', newPasswordConfirmation = ''"
+      @signout-ok-sign="deleteUser"
+      @deleteUser-ok-sign="$router.push({ name: 'MainPage' })"
     />
   </v-container>
 </template>
@@ -235,7 +238,7 @@ export default {
   },
   data: function () {
     return {
-      userId: 11,
+      userId: 9,
       profileImageFile: null,
       profileImageSrc: '',
       nickname: 'nickname',
@@ -362,20 +365,80 @@ export default {
           modalBtn.click()
         })
     },
-    // signOut: function () {
-    //   axios({
-    //     method: 'delete',
-    //     url: `http://localhost/qwert/accounts/${this.userId}/`,
-    //     headers: this.setToken()
-    //   })
-    //     .then(res => {
-    //       console.log(res)
-    //     })
-    //     .catch(err => {
-    //       console.log(err)
-    //     })
-    // }
+    // 회원 탈퇴 확인을 위한 모달 창을 띄우는 함수
+    signout: function () {
+      this.modalMsg = {
+        name: 'signout',
+        triggerBtn: '',
+        title: '회원 탈퇴',
+        text: '정말로 회원 탈퇴를 진행하고<br/>계정을 삭제하시겠습니까?',
+        positiveBtn: '확인',
+        negativeBtn: '취소',
+      }
+      const modalBtn = document.querySelector('#modalBtn')
+      modalBtn.click()
+    },
+    // 회원 탈퇴 함수
+    deleteUser: function () {
+      axios({
+        method: 'delete',
+        url: `http://localhost/qwert/accounts/${this.userId}/`,
+        headers: { token: localStorage.getItem('jwtToken') }
+      })
+        .then(res => {
+          console.log(res)
+          // jwt 토큰 삭제
+          localStorage.removeItem('jwtToken')
+          // 회원 탈퇴 성공 모달 창
+          this.modalMsg = {
+            name: 'deleteUser',
+            triggerBtn: '',
+            title: '',
+            text: '회원 탈퇴가 완료되었습니다.',
+            positiveBtn: '확인',
+            negativeBtn: '',
+          }
+          const modalBtn = document.querySelector('#modalBtn')
+          modalBtn.click()
+        })
+        .catch(err => {
+          console.log(err)
+          // 회원 탈퇴 실패 모달 창 (상태 코드에 따라)
+          if (err.response.status === 401) {
+            this.modalMsg = {
+              name: 'deleteUserFailure401',
+              triggerBtn: '',
+              title: `${err.response.status} error`,
+              text: '인증되지 않은 사용자입니다.<br/>혹은<br/>현재 로그인된 상태인지 확인해주세요.',
+              positiveBtn: '확인',
+              negativeBtn: '',
+            }
+          } else if (err.response.status === 404) {
+            this.modalMsg = {
+              name: 'deleteUserFailure404',
+              triggerBtn: '',
+              title: `${err.response.status} error`,
+              text: '확인할 수 없는 사용자에 대한 요청입니다.',
+              positiveBtn: '확인',
+              negativeBtn: '',
+            }
+          } else {
+            this.modalMsg = {
+              name: 'deleteUserFailure',
+              triggerBtn: '',
+              title: `${err.response.status} error`,
+              text: '제한된 접근입니다.<br/>현재 로그인된 상태인지 확인해주세요.',
+              positiveBtn: '확인',
+              negativeBtn: '',
+            }
+          }
+          const modalBtn = document.querySelector('#modalBtn')
+          modalBtn.click()
+        })
+    },
   },
+  // '새 비밀번호'와 '새 비밀번호 확인'이 일치하여 경고가 없는 상태에서 '새 비밀번호'를 바꾸면 일치하지 않는다는 경고가 뜨지 않는 문제 해결
+  // '새 비밀번호'를 바꿨을 때 '새 비밀번호 확인'에서도 즉각 반응하여 경고를 나타낼 수 있도록 하는 일종의 꼼수
   watch: {
     newPassword: function () {
       return new Promise((resolve) => {
