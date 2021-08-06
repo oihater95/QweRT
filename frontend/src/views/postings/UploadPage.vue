4df6a8a5dabc4ffd1af6a15b3b4400ad5e1fc2d9
문성호 <msungho9512@naver.com>
2021-08-06 10:11:46 +0900

    code : 제목, 내용 추가

--- frontend/src/views/postings/UploadPage.vue
+++ frontend/src/views/postings/UploadPage.vue
@@ -6,13 +6,20 @@
         <div class="container">
           <v-card class="my-5 posting-title">
             <v-text-field
-              label="제목"
+              label="Title"
               :rules="rules"
               hide-details="auto"
             ></v-text-field>
           </v-card>
           <v-card class="my-5 posting-content">
-            <v-text-field label="내용"></v-text-field>
+            <v-textarea
+            >
+              <template v-slot:label>
+                <div>
+                  content <small></small>
+                </div>
+              </template>
+            </v-textarea>
           </v-card>
         </div>
       </v-col>
