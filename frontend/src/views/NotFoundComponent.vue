e7a8462683b94a12aef5e8af70444092ecfb0a1c
오승철 <seung941226@gmail.com>
2021-08-10 05:45:34 +0900

    code : 네비게이션 가드 설정, 404에러 페이지, 네브바 일부 에러 수정

---
+++ frontend/src/views/NotFoundComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="not-found__div">
+      <h1>페이지를 찾을 수 없습니다.</h1>
+      <h3>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</h3>
+  </div>
+</template>
+
+<script>
+import "@/css/NotFoundComponent.scss"
+
+export default {
+    name: "NotFoundComponent"
+}
+</script>
+
+<style>
+
+</style>
